f0321ef4fcc17ceccfaa0270a39f3bfe395f59a9
Joel Sprinkle <joel.sprinkle@carleton.ca>
2016-09-15 17:50:50 -0400

    minor

--- src/musicXML/Main.java
+++ src/musicXML/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("musicXML.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 350, 275));
         primaryStage.show();
     }
 
